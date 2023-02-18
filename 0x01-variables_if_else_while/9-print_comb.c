f17a61b5d45b76658da6d49cee5bd0510d62acd3
FaresRez <fers.rez16@gmail.com>
2023-02-18 10:58:30 -0800

    $1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+		if (c != 9)
+			putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
