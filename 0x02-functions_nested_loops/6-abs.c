a7f765cde8417e91b9e48e7af25427880153781f
FaresRez <fers.rez16@gmail.com>
2023-02-21 14:46:27 -0800

    $1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: the number
+ *
+ * Return: the abs of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n*=-1;
+	
+	return(n);
+}
