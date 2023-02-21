90c04bf66dff91bc545984bba385c18ae2637e45
FaresRez <fers.rez16@gmail.com>
2023-02-21 13:48:29 -0800

    $1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,10 @@ void print_alphabet_x10(void)
 	char c;
 
 	for (i = 1; i <= 9; i++)
-		for (c = 'a'; c >= 'z'; c++)
+	{
+		_putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
+	}
 	_putchar('\n');
 }
