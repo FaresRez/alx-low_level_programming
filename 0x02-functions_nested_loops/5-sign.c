ddedbbb0d1b5df2f9c636612e7538dcffa502b97
FaresRez <fers.rez16@gmail.com>
2023-02-21 14:37:24 -0800

    $1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_sign - print the signe of a given number
+ * @n: the number checked
+ *
+ * Return: 1 and + if >0 OR 0 and 0 if =0 OR -1 and - if <0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+                return (-1);
+	}
+}
