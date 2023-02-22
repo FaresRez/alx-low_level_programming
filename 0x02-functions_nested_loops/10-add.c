1035ca53d85adddcd26eaea7aa34e4a2fe4dd829
FaresRez <fers.rez16@gmail.com>
2023-02-22 08:52:43 -0800

    $1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @n: the first number
+ * @c: the second number
+ *
+ * Return: sum of the two numbers
+ */
+
+int add(int n, int c)
+{
+	return (n + c);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	char ch[5] = "00:00";
+	for (i = 0; i < 60; i++)
+		_putchar
+	       for (j = 0; j > 60; j++)
+	       if (i < 0 || j < 0)
+			_putchar('0');	       
+	return (n);
+}
