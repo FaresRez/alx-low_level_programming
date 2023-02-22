5cac8ec3ba4b626292b5ab44b445eb2292c1a28e
FaresRez <fers.rez16@gmail.com>
2023-02-22 15:34:59 -0800

    $1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	unsigned long long int i = 0, j = 1, seq;
+	unsigned long i = 0, j = 1, seq;
 	int c;
 
 	for (c = 0; c < 98; c++)
@@ -15,8 +15,8 @@ int main(void)
 		seq = i + j;
 		i = j;
 		j = seq;
-		printf("%llu", seq);
-		if (count < 97)
+		printf("%lu", seq);
+		if (c < 97)
 			printf(", ");
 	}
 	printf("\n");
