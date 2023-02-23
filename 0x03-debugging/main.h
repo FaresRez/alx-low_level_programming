d65661aea9409a30ff3743d75d78672c6a357a4b
FaresRez <fers.rez16@gmail.com>
2023-02-23 04:35:56 -0800

    $1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_FILE
-#define MAIN_FILE
+#ifndef MAIN_H
+#define MAIN_H
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
