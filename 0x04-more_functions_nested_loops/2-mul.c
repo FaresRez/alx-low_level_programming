a7ec9cdf569321253d8baf341a6f1063de04f3d4
FaresRez <fers.rez16@gmail.com>
2023-02-23 06:25:10 -0800

    $1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * mul - checks for lowercase character
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: multiple a and b
+ */
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
