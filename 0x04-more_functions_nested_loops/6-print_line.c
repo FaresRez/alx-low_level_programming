9b1027bf3876763025b208aceb15e866f132a58a
FaresRez <fers.rez16@gmail.com>
2023-02-24 03:52:41 -0800

    $1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_line - print a line using putchar
+ *@n: line length
+ *
+ * Return: line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0) 
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	_putchar('\n');
+
+}
