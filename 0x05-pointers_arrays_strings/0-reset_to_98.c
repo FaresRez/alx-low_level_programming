586049b6d899173eff83850b30226e27a935fa65
FaresRez <fers.rez16@gmail.com>
2023-02-27 12:53:35 -0800

    $1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a value using pointers
+ * @n: pointer to n
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
