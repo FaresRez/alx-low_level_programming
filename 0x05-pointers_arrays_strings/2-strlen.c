1a4598c3f96f9dca40332c66a39a22e29428d8ff
FaresRez <fers.rez16@gmail.com>
2023-02-27 13:33:40 -0800

    $1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen - length of a string using pointers
+ * @s: pointer
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(a + i) != 0)
+	{
+		++i;
+	}
+	return (i);
+}
