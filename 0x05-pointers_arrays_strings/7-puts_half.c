cd9bb6982e9e9b155adb2842b8c87fd221403773
FaresRez <fers.rez16@gmail.com>
2023-02-28 13:08:12 -0800

    $1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * puts_half - print half a string using pointers
+ * @str: pointer
+ *
+ * Return: half string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(str + i) != 0)
+	{
+		++i;
+	}
+	j = (i) / 2 ;
+	while (j < i)
+	{
+		_putchar(*(str + j));
+		++j;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void print_array(int *a, int n);
+void puts_half(char *str);
 #endif
