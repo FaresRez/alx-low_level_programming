79f03ebd5a6e68ce17dfe2e45109a87bfa3fc3b1
FaresRez <fers.rez16@gmail.com>
2023-02-28 13:42:57 -0800

    $1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - copy a string in another one using pointers
+ * @dest: pointer of source string
+ * @src: pointer of destination string
+ *
+ * Return: a pointer to the copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (*src != '\0')
+	{
+		*dest = *src_ptr;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ void _puts(char *str);
 void print_rev(char *s);
 void print_array(int *a, int n);
 void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
 #endif
