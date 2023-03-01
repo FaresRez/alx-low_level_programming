bab4b08638d922408937062a2c790484c439df9e
FaresRez <fers.rez16@gmail.com>
2023-03-01 13:29:17 -0800

    $1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concat 2 strings using pointers
+ * @dest: pointer to string 1
+ * @src: pointer to string 2
+ *
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
