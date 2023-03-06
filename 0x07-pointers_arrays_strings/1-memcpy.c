3307245c83d2930f6ff7edc5f707eaddb4b11210
FaresRez <fers.rez16@gmail.com>
2023-03-06 14:04:29 -0800

    $1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * *_memcpy - copy n bytes in memory  using pointers
+ * @dest: pointer to string
+ * @src: the value inseret
+ * @n: the nb of bytes allocated
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n && *(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (s);
+}
