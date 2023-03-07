1c67d7b216b55df350576688dd7ae609fde672d7
FaresRez <fers.rez16@gmail.com>
2023-03-06 17:33:35 -0800

    $1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,7 @@
  * Return: print sum of diagonals
  */
 
-char print_diagsums(int *a, int size)
+void print_diagsums(int *a, int size)
 {
 	int i, j, d1 = 0, d2 = 0;
 
@@ -19,10 +19,10 @@ char print_diagsums(int *a, int size)
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				d1 += a[i][j];
+				d1 += *(a + i * size + j);
 			if (i + j == size - 1)
-				d2 += a[i][j];
+				d2 += *(a + i * size + j);
 		}
 	}
-	printf("%d\, %d", d1, d2);
+	printf("%d, %d\n", d1, d2);
 }
