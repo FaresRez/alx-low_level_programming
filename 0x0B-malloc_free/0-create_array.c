94f194a58514526fce698c5897723e70ce951629
FaresRez <fers.rez16@gmail.com>
2023-03-13 06:55:30 -0700

    $1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,14 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ar;
+	int i;
 
 	if (size == 0)
 		return (NULL);
 	ar = malloc(size * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
-	ar[0] = c;
+	for (i = 0; i < size; i++)
+		ar[i] = c;
 	return (ar);
 }
