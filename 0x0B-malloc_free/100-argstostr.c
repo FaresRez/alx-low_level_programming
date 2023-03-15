712193a782cc30048f25c95ff76d10050678e2fe
FaresRez <fers.rez16@gmail.com>
2023-03-15 06:47:31 -0700

    $1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,6 +10,38 @@
  *
  * Return: a pointer or NULL
  */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = malloc(1);
+		*s1 = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = malloc(1);
+		*s2 = '\0';
+	}
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	p = malloc(sizeof(char) * (i + j + 1));
+	if (p == NULL)
+		return (NULL);
+	while (*s1)
+		*p++ = *s1++;
+	while (*s2)
+		*p++ = *s2++;
+	*p = '\0';
+	p -= i + j;
+	return (p);
+
+}
+
 char *argstostr(int ac, char **av)
 {
 	int i;
