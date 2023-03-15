7b2b3c1908f2f1158c93ed22c090cb39b1cceea3
FaresRez <fers.rez16@gmail.com>
2023-03-15 03:43:28 -0700

    $1

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,9 +12,10 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
+	int i,j;
 
 	for (i = 0; i < height; i++)
-		free(&grid[i]);
+		for (j = 0; j < height; j++)
+			free(&grid[i][j]);
 	free(grid);
 }
