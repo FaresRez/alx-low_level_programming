4cb7a490f1371ea63d2a809514242c67b22c6f68
FaresRez <fers.rez16@gmail.com>
2023-03-31 08:17:06 -0700

    Writing a constructor that print a string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - a constructor
+ *
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
