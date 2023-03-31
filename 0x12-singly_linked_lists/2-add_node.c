e210c362dc661949da1bf5920722d76c30595005
FaresRez <fers.rez16@gmail.com>
2023-03-31 05:39:35 -0700

    Add len of the data to the node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,11 +14,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+	int i = 0;
 
 	if (new == NULL)
 		return (NULL);
+	while(str[i])
+		i++;
 	new->str = strdup(str);
 	new->next = *head;
+	new->len = i;
 	*head = new;
 	return (*head);
 }
