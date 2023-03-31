288905e25180e9d305385199a0aa988eaa9468f1
FaresRez <fers.rez16@gmail.com>
2023-03-31 08:03:12 -0700

    Fix betty style

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,18 +4,19 @@
 #include "lists.h"
 
 /**
- * print_list -  print all the node of a linked list
- * @h: pointer to a node
+ * free_list -  free a linked list
+ * @head: pointer to a node
  *
- * Return: number of nodes
+ * Return: void
  */
 void free_list(list_t *head)
-{	
+{
 	list_t *h = head;
 
 	while (h != NULL)
-	{	
+	{
 		list_t *p = h;
+
 		h = h->next;
 		free(p->str);
 		free(p);
