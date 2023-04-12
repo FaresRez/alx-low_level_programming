4fc6f8bf1eddb46216a53d0ce7f605132c3aad1a
FaresRez <fers.rez16@gmail.com>
2023-04-11 18:54:33 -0700

    Fix the NULL case

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,11 +14,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int j = 0, c = 1, sum = 0;
 	int i;
 
+	if (b == NULL)
+		return (0);
 	while (b[j + 1] != '\0')
 		j++;
 	for (i = j; i >= 0; i--)
 	{
-		if ((b[i] != '0' && b[i] != '1') || b == NULL)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
 			sum += c;
