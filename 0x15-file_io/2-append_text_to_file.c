6c8822f718d2396d3a7084dd376e0e910cb7769f
FaresRez <fers.rez16@gmail.com>
2023-04-13 17:26:55 -0700

    Fix betty style

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: the adresse of a file
+ * @text_content: the data to write in the file
+ *
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
