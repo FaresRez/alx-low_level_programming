89d62a0793f98646d247402b43df3e1010cca790
FaresRez <fers.rez16@gmail.com>
2023-06-19 10:48:02 +0000

    Fix the command

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -g -fPIC -Wall -Werror -Wextra -pedantic std=gnu89 *.c -shared -o liball.so
+gcc -g -fPIC -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -shared -o liball.so
 cp liball.so /usr/lib
