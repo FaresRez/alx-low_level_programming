f229601c3aa9c6c4c6838af39a24cfbe538d7abb
FaresRez <fers.rez16@gmail.com>
2023-09-09 15:12:45 +0000

    Fixing betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * hash_table_create(hash_table_create - create an empty hash table
+ *
+ *@size: size of the table
+ * Return: hash table pointer
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+
 	if (hash_table == NULL)
-                return NULL;
+		return (NULL);
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return NULL;
+		return (NULL);
 	}
 	hash_table->size = size;
 
-	return hash_table;
+	return (hash_table);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * key_index - give the index of the key
+ *
+ *@key: the pointer of the key
+ *@size: size of the hash table
+ * Return: index key.
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return hash_djb2(key) % size;
+	return (hash_djb2(key) % size);
 }
